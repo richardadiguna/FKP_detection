2e7bc7921a131d09c9bffe1862b304d7e2963690
Richard Adiguna <richardadiguna17@gmail.com>
2020-06-12 20:44:28 +0700

    feat: Add new ToTensor to convert numpy array to toech tensor

--- data_loader.py
+++ data_loader.py
@@ -95,3 +95,23 @@ class RandomCrop(object):
         key_pts = key_pts - [left, top]
 
         return {'image': image, 'keypoints': key_pts}
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, key_pts = sample['image'], sample['keypoints']
+
+        # if image has no grayscale color channel, add one
+        if(len(image.shape) == 2):
+            # add that third color dim
+            image = image.reshape(image.shape[0], image.shape[1], 1)
+ 
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+
+        return {'image': torch.from_numpy(image),
+                'keypoints': torch.from_numpy(key_pts)}
